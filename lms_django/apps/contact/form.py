cf74028bf8e71aec1b2afa45e1ea0b95dd0253e3
Syed Ittyhadul Islam <az.walker30822@gmail.com>
2025-08-06 17:57:41 +0600

    post method

--- lms_django/apps/contact/form.py
+++ lms_django/apps/contact/form.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    customer_id = forms.CharField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea())
+    customer_id = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Enter CID"}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Enter First Name"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Enter Last Name"}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder" : "Enter Email"}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"placeholder" : "Leave your message"}))
     
--- lms_django/apps/contact/views.py
+++ lms_django/apps/contact/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from . form import ContactForm
 # Create your views here.
 def contactForm(request):
-    form = ContactForm(auto_id="contact_%s")
+    if request.method == "POST":
+        form = ContactForm(request.POST, auto_id="contact_%s") #initial={"email" : "example@gmail.com"}
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = ContactForm()
     return render(request, "contact/contact.html", {"form": form})
